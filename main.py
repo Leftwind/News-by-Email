da22b0dfedd3c8a03e53232d043c3aebddfa05bf
Leftwind <andreselfisico@gmail.com>
2023-11-21 13:42:42 +0100

    Send mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #We don't need graphical user interface. Only Command User Interface. 
 import requests
+from send_email import send_email
 
 api_key='6df06dce864941e58549c6f084c62336'
 api_url='https://newsapi.org/v2/everything?domains=wsj.com&apiKey=6df06dce864941e58549c6f084c62336'
@@ -10,6 +11,12 @@ request = requests.get(api_url)
 #Get dictionary
 content = request.json()
 #Acces the article titles and description
+body = ''
 for articles in content["articles"]:
-    print(articles["title"])
-    print(articles["description"])
+    #Create articles to send message:
+    #We need the titles and the descriptioons to contain the data.   
+    body = body + articles["title"] + '\n' + articles["description"] + 2*'\n'
+    
+body = body.encode('utf-8')
+#Out of the loop cause 1 email not a lot. 
+send_email(message = body)
